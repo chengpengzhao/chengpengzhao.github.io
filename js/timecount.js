9d979546b592e24ce73c641bb6a7818c7b54d663
chengpengzhao <chengpengzhao@users.noreply.github.com>
2024-12-21 03:58:36 +0000

    deploy: chengpengzhao/hexo-theme-matery@f9b13027fe4b84a526a6495c315725c85360467a

--- js/timecount.js
+++ js/timecount.js
@@ -1,7 +1,7 @@
 var now = new Date().getTime() // 当前时间戳
-var postinfo = document.getElementsByClassName("post-date")[0]
+//var postinfo = document.getElementsByClassName("post-date")[1]
 //var postinfo__date_created = new Date(postinfo.innerText.split(" ")[1])//根据发布时间
-var postinfo__date_created = new Date(postinfo.children[1].innerText.split(" ")[1])//根据更新时间
+var postinfo__date_created = new Date(document.getElementsByClassName("post-date")[1].innerText.split(" ")[2])//根据更新时间
 postinfo__date_created = postinfo__date_created.getTime() // 文章发布时间戳
 //console.log("文章创建时间",postinfo__date_created)
  
@@ -10,5 +10,5 @@ var datepassing = parseInt(now - postinfo__date_created)
 // 超过31天提示：3600*24*31*1000
 if(datepassing > 2678400000){
     var out = parseInt(datepassing / 86400000)
-    document.write("<div class=\"note info\"><p><strong>文章时效性提示</strong><br>这是一篇创建于 " + out + " 天前的文章，其中的信息可能已经有所发展或是发生改变。</p></div>")
+    document.write("<div class=\"note info\"><blockquote><strong>文章时效性提示</strong><br>这是一篇创建于 " + out + " 天前的文章，请注意⚠️，：其中的信息可能已发生变化或有所更新，部分内容可能不再完全适用当前的情况。</blockquote></div>")
 }
